babd67d96d28b946dd4a7a7db85a704a6ce517c5
Oliver Kucharzewski <oliver.kucharzewski@nosto.com>
2023-12-30 13:29:05 +1100

    Fix cicd

--- jest.config.ts
+++ jest.config.ts
@@ -1,18 +1,19 @@
 module.exports = {
-  rootDir: './',
-  testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['<rootDir>/jest-setup.js'],
+  rootDir: "./",
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest-setup.js"],
   collectCoverage: true,
   collectCoverageFrom: [
-    '<rootDir>/src/**/*.{ts,tsx}',
-    '<rootDir>/node_modules/',
-    '!<rootDir>/src/common/background/**',
-    '!<rootDir>/src/index.ts'
+    "<rootDir>/src/**/*.{ts,tsx}",
+    "<rootDir>/node_modules/",
+    "!<rootDir>/src/common/background/**",
+    "!<rootDir>/src/index.ts",
   ],
-  preset: 'ts-jest',
+  preset: "ts-jest",
   transform: {
-    '^.+\\.(ts|tsx)?$': 'ts-jest'
+    "^.+\\.(ts|tsx)?$": "ts-jest",
+    "^.+\\.(js|jsx)$": "babel-jest",
   },
-  coverageReporters: ['json', 'html', 'text'],
-  transformIgnorePatterns: ['node_modules/(?!@?lit)']
-}
+  coverageReporters: ["json", "html", "text"],
+  transformIgnorePatterns: ["node_modules/(?!@?lit)"],
+};
